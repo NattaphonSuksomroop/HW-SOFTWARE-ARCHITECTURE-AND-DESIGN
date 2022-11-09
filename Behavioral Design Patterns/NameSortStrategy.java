b25036d7efee4da74c7dbc734e9d95c784c58f6d
63015049_Nattaphon Suksomroop <88423052+NattaphonSuksomroop@users.noreply.github.com>
2022-11-09 21:26:38 +0700

    Add files via upload

---
+++ Behavioral Design Patterns/NameSortStrategy.java
@@ -0,0 +1,14 @@
+package edu.parinya.softarchdesign.behavioral2;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class NameSortStrategy implements SortStrategy {
+    @Override
+    public void customSort(List<Person> people) {
+        // YOU ARE NOT ALLOWED TO MODIFY THE CODE ABOVE THIS COMMENT !!
+        // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
+        people.sort(Comparator.comparing(Person::getName));
+    }
+
+}
